272f4d17398d4f96f11737de6ae04b6bd4616a92
Jayadevi2000 <46923520+Jayadevi2000@users.noreply.github.com>
2020-11-17 19:34:42 +0100

    Update HiloPlantilla2.java

--- Hilos_PSP/src/algoritmos/HiloPlantilla2.java
+++ Hilos_PSP/src/algoritmos/HiloPlantilla2.java
@@ -15,34 +15,45 @@ public class HiloPlantilla2 extends Thread{
 		        this.panel = panel;
 		        this.array = array;
 	  }
-	//Método de ordenación
+	//MÃ©todo de ordenaciÃ³n
 			public void run() {
 				int i, j;
 				int aux;
-				for (i = 1; i < array.length; i++)
-				{
+				for (i = 1; i < array.length; i++){
 				/* indice j explora la sublista a[i-1]..a[0] buscando la
 				posicion correcta del elemento destino, lo asigna a a[j] */
 				j = i;
 				aux = array[i];
-				/* se localiza el punto de inserción explorando hacia abajo */
+				/* se localiza el punto de inserciÃ³n explorando hacia abajo */
 				while (j > 0 && aux < array[j-1])
 				{
 				/* desplazar elementos hacia arriba para hacer espacio */
 					array[j] = array[j-1];
 				j--;
+				
+		
+				 ((JButton) panel.getComponent(j)).setText(Integer.toString(array[j]));
+					((JButton)panel.getComponent(j)).setBackground(Color.YELLOW);
+				/*	try {
+						Thread.sleep(500);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}*/
 				}
-				try {
-					 ((JButton) panel.getComponent(j)).setText(Integer.toString(array[j]));
-						((JButton)panel.getComponent(j)).setBackground(Color.YELLOW);
-					Thread.sleep(500);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+				
 				array[j] = aux;
+				 ((JButton) panel.getComponent(j)).setText(Integer.toString(array[j]));
+					((JButton)panel.getComponent(j)).setBackground(Color.YELLOW);
 				
 				}
+		
+				for (int x=0;x<array.length;x++) {
+					((JButton) panel.getComponent(x)).setText(Integer.toString(array[x]));
+					((JButton)panel.getComponent(x)).setBackground(Color.YELLOW);
+				
+				}
+				
 				}
 			
 }
